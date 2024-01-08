632b01ac8649e059a1f890681a901c72be59fa61
noprobelm <noprobelm@protonmail.com>
2024-01-08 16:40:51 -0500

    ProcessTable now has basic functionality
    - Cells will now update each time system processes are queried
    - New processes will be added
    
    Need to add capability to remove a row after its PID has been dropped.

--- pytop/widgets.py
+++ pytop/widgets.py
@@ -1,4 +1,5 @@
 from textual.widgets import DataTable
+from textual.widgets._data_table import RowDoesNotExist
 from . import parser
 import psutil
 from textual.reactive import reactive
@@ -22,10 +23,10 @@ class ProcessTable(DataTable):
             ]
         )
     }
-    generation = reactive(0)
+    generation = reactive(processes)
 
     def on_mount(self):
-        self.add_columns(
+        for label in (
             "PID",
             "USER",
             "PRI",
@@ -37,11 +38,14 @@ class ProcessTable(DataTable):
             "CPU%",
             "MEM%",
             "TIME+",
-            "COMMAND",
-        )
+            "Command",
+        ):
+            self.add_column(label, key=label)
         for pid in self.processes:
             virt, res, shr = parser.parse_pmem(self.processes[pid]["memory_info"])
             cpu_times = parser.parse_pcputimes(self.processes[pid]["cpu_times"])
+            cmdline = parser.parse_cmdline(self.processes[pid]["cmdline"])
+
             self.add_row(
                 pid,
                 self.processes[pid]["username"],
@@ -54,6 +58,66 @@ class ProcessTable(DataTable):
                 self.processes[pid]["cpu_percent"],
                 self.processes[pid]["memory_percent"],
                 cpu_times,
-                parser.parse_cmdline(self.processes[pid]["cmdline"])
-                or self.processes[pid]["name"],
+                cmdline or self.processes[pid]["name"],
+                key=str(pid),
             )
+
+        self.set_interval(1.5, self.update_processes)
+
+    def update_processes(self):
+        self.processes = {
+            p.pid: p.info
+            for p in psutil.process_iter(
+                [
+                    "pid",
+                    "name",
+                    "username",
+                    "nice",
+                    "memory_info",
+                    "memory_percent",
+                    "status",
+                    "cpu_percent",
+                    "cpu_times",
+                    "cmdline",
+                ]
+            )
+        }
+        for pid in self.processes:
+            row_key = str(pid)
+            try:
+                self.get_row(str(row_key))
+            except RowDoesNotExist:
+                virt, res, shr = parser.parse_pmem(self.processes[pid]["memory_info"])
+                cpu_times = parser.parse_pcputimes(self.processes[pid]["cpu_times"])
+                self.add_row(
+                    pid,
+                    self.processes[pid]["username"],
+                    self.processes[pid]["nice"],
+                    self.processes[pid]["nice"],
+                    virt,
+                    res,
+                    shr,
+                    self.processes[pid]["status"],
+                    self.processes[pid]["cpu_percent"],
+                    self.processes[pid]["memory_percent"],
+                    cpu_times,
+                    parser.parse_cmdline(self.processes[pid]["cmdline"])
+                    or self.processes[pid]["name"],
+                )
+            else:
+                virt, res, shr = parser.parse_pmem(self.processes[pid]["memory_info"])
+                cpu_times = parser.parse_pcputimes(self.processes[pid]["cpu_times"])
+                cmdline = parser.parse_cmdline(self.processes[pid]["cmdline"])
+                self.update_cell(row_key, "PRI", self.processes[pid]["nice"])
+                self.update_cell(row_key, "NI", self.processes[pid]["nice"])
+                self.update_cell(row_key, "VIRT", virt)
+                self.update_cell(row_key, "RES", res)
+                self.update_cell(row_key, "SHR", shr)
+                self.update_cell(row_key, "S", self.processes[pid]["status"])
+                self.update_cell(row_key, "CPU%", self.processes[pid]["cpu_percent"])
+                self.update_cell(row_key, "MEM%", self.processes[pid]["memory_percent"])
+                self.update_cell(
+                    row_key,
+                    "Command",
+                    cmdline or self.processes[pid]["name"],
+                )
