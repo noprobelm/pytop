cbea7a386e78ccc2ba7912ba80699a4b267cf8ca
noprobelm <noprobelm@protonmail.com>
2024-01-07 23:40:33 -0500

    First commit

---
+++ pytop/app/app.py
@@ -0,0 +1,13 @@
+from textual.app import App, ComposeResult, ComposeResult
+from textual.reactive import reactive
+from textual.widgets import Footer, Static
+
+from pathlib import Path
+from typing import Iterable
+from textual.widgets import Input
+from textual.validation import URL
+
+from textual.app import App, ComposeResult
+from textual.widgets import DirectoryTree
+from textual.binding import Binding
+from textual.screen import Screen
