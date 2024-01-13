738d654968f4765ca2a6b73716911753bc707ca8
noprobelm <noprobelm@protonmail.com>
2024-01-13 03:44:53 -0500

    Modified 'used' to round to 2nd place

--- pytop/meter.py
+++ pytop/meter.py
@@ -56,30 +56,12 @@ class TextProgressBar(Widget):
         self.left_bound = "["
         self.right_bound = "]"
 
-    def _convert_units(self, data: int | float):
-        units = {0: "K", 1: "K", 2: "M", 3: "G", 4: "T"}
-        unit_key = 0
-        while data > 1024:
-            data = data // 1024
-            unit_key += 1
-
-        if unit_key == 0:
-            return "0K"
-        elif unit_key == 4:
-            return f">{str(round(data, 2))}{units[unit_key]}"
-        else:
-            return f"{str(round(data, 2))}{units[unit_key]}"
-
     def compute__readout(self) -> str:
         if self.progress is None or self.total is None:
             return f"-/-"
         match self.readout_type:
             case "percent":
                 return f"{self.progress}%"
-            case "memory":
-                used = self._convert_units(self.progress)
-                total = self._convert_units(self.total)
-                return f"{used}/{total}"
             case "proportion":
                 return f"{self.progress}/{self.total}"
 
@@ -163,7 +145,7 @@ class MemoryUsage(TextProgressBar):
         if unit_key == 0:
             return "0K"
         elif unit_key == 4:
-            return f">{str(round(data, 1))}{units[unit_key]}"
+            return f">{str(round(data, 2))}{units[unit_key]}"
         else:
             return f"{str(round(data, 1))}{units[unit_key]}"
 
