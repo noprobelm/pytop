16064148fb4364340a6e0ba3c24783bf843cede6
noprobelm <noprobelm@protonmail.com>
2024-01-13 13:20:57 -0500

    Documented and refactored some meters

--- pytop/widgets/meters.py
+++ pytop/widgets/meters.py
@@ -5,35 +5,44 @@ from time import time
 
 
 class LoadAverage(Static):
-    one: Reactive[float] = Reactive(0.0)
-    five: Reactive[float] = Reactive(0.0)
-    fifteen: Reactive[float] = Reactive(0.0)
+    """A meter for displaying 1, 5, and 15 minute CPU load averages"""
 
-    def on_mount(self):
-        self.set_interval(1.5, self.update_averages)
+    load_avg: Reactive[tuple] = Reactive(psutil.getloadavg)
 
-    def update_averages(self):
-        load_avg = psutil.getloadavg()
-        self.one, self.five, self.fifteen = load_avg[0], load_avg[1], load_avg[2]
+    def on_mount(self) -> None:
+        self.set_interval(1.5, self.update_load_avg)
 
-    def render(self):
+    def update_load_avg(self) -> None:
+        self.load_avg = psutil.getloadavg()
+
+    def watch_loadavg(self) -> None:
+        self.update()
+
+    def render(self) -> str:
+        self.one, self.five, self.fifteen = (
+            self.load_avg[0],
+            self.load_avg[1],
+            self.load_avg[2],
+        )
         return f"Load average: {round(self.one, 2)} {round(self.five, 2)} {round(self.fifteen, 2)}"
 
 
 class Uptime(Static):
+    """A meter for displaying time elapsed since system boot"""
+
     boot_time = psutil.boot_time()
     current_time: Reactive[float] = Reactive(time)
 
-    def on_mount(self):
+    def on_mount(self) -> None:
         self.set_interval(1.5, self.update_time)
 
-    def update_time(self):
+    def update_time(self) -> None:
         self.current_time = time()
 
-    def watch_current_time(self):
+    def watch_current_time(self) -> None:
         self.update()
 
-    def render(self):
+    def render(self) -> str:
         uptime_seconds = self.current_time - self.boot_time
         m, s = divmod(uptime_seconds, 60)
         h, m = divmod(m, 60)
