712e89c28ecd3c6141f5d155c67cb0cc84c0bdb7
noprobelm <noprobelm@protonmail.com>
2024-01-18 14:18:57 -0500

    Removed label positional arg for CPUUsage

--- pytop/widgets/_cpu_usage.py
+++ pytop/widgets/_cpu_usage.py
@@ -1,26 +1,17 @@
 from ._text_progress_bar import TextProgressBar
-from textual.reactive import Reactive
 
 
 class CPUUsage(TextProgressBar):
     """A meter for displaying CPU usage (per core) as a text progress meter"""
 
-    DEFAULT_CSS = """
-    CPUUsage {
-        height: 1;
-        width: 1fr;
-    }
-    """
-
     def __init__(
         self,
-        label: str,
         core: int,
         *,
         name: str | None = None,
         id: str | None = None,
         classes: str | None = None,
     ):
-        self.label = label
         self.core = core
-        super().__init__(label, "percent", name=name, id=id, classes=classes)
+        self.label = str(self.core)
+        super().__init__(str(core), "percent", name=name, id=id, classes=classes)
--- pytop/widgets/_meter_header.py
+++ pytop/widgets/_meter_header.py
@@ -10,18 +10,18 @@ class MeterHeader(Horizontal):
         with Vertical(id="c1"):
             for i in range(num_cores // 2)[::2]:
                 with Horizontal(classes="meter-row"):
-                    yield CPUUsage(str(i), i, classes="meter")
-                    yield CPUUsage(str(i + 1), i + 1, classes="meter")
+                    yield CPUUsage(i, classes="meter")
+                    yield CPUUsage(i + 1, classes="meter")
 
-            yield Horizontal(RAMUsage("Mem"), classes="meter-row")
-            yield Horizontal(SwapUsage("Swp"), classes="meter-row")
+            yield Horizontal(RAMUsage("Mem", classes="meter"), classes="meter-row")
+            yield Horizontal(SwapUsage("Swp", classes="meter"), classes="meter-row")
 
         with Vertical(id="c2"):
             for i in range(num_cores // 2, num_cores)[::2]:
                 with Horizontal(classes="meter-row"):
-                    yield CPUUsage(str(i), i, classes="meter")
-                    yield CPUUsage(str(i + 1), i + 1, classes="meter")
+                    yield CPUUsage(i, classes="meter")
+                    yield CPUUsage(i + 1, classes="meter")
 
-            yield Horizontal(Tasks(), classes="meter-row")
-            yield Horizontal(LoadAverage(), classes="meter-row")
-            yield Horizontal(Uptime(), classes="meter-row")
+            yield Horizontal(Tasks(classes="meter"), classes="meter-row")
+            yield Horizontal(LoadAverage(classes="meter"), classes="meter-row")
+            yield Horizontal(Uptime(classes="meter"), classes="meter-row")
